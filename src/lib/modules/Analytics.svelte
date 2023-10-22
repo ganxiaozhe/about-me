bfb052d85afe9a4122329e31dab0851db61b7ed2
ganxiaozhe <hi@gxzv.com>
2023-10-22 16:54:50 +0800

    analytics add

---
+++ src/lib/modules/Analytics.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<!-- 51.LA Analytics -->
+	<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
+	<script>
+		if (typeof __AnalyTimer == 'undefined') {
+			let __AnalyTimer = setInterval(() => {
+				if (!LA || !LA.init) {return;}
+
+				LA.init({id:"KEIxrFsbADmTVb9W",ck:"KEIxrFsbADmTVb9W"});
+				clearInterval(__AnalyTimer);
+			}, 500);
+		}
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang='ts'>
+  import { dev } from '$app/environment';
+	import Analytics from '$lib/modules/Analytics.svelte';
   import '../app.css';
 </script>
+
+{#if !dev}
+<Analytics />
+{/if}
 
 <slot />
